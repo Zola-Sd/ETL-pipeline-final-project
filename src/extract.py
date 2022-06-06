32f19046a5cdf88ec66f633c9b7fd2b82c2e5fb6
cte88 <camerontee88@gmail.com>
2022-06-06 15:58:13 +0100

    minor changes

--- src/extract.py
+++ src/extract.py
@@ -1,16 +1,11 @@
 import csv
-from pathlib import Path
-
-
-def fetch_filepath(filename):
-    return Path(__file__).parent/filename
 
 def extract_data():
-    with open(fetch_filepath('test.csv'), 'r') as f:
+    with open('test.csv', 'r') as f:
         contents = csv.DictReader(f)
 
         sales_data = [row for row in contents]
     
     return sales_data
 
-print(extract_data())
+sales_data = extract_data()
