1c1b4986b22c2ae78bc9dcafd621e7dc9d0d5a00
cte88 <camerontee88@gmail.com>
2022-06-07 15:49:43 +0100

    added transaction script

--- src/sql_script.py
+++ src/sql_script.py
@@ -5,11 +5,54 @@ db_password="password"
 db_name="postgres"
 
 connection = db_connect.connect(host=host_name,user=db_user,password=db_password,database=db_name)
- 
 cursor = connection.cursor()
 
-sql= ''' CREATE TABLE IF NOT EXISTS store_table (Branch_Code varchar, Branch_Name varchar);'''
+def create_store_table():
+    sql= \
+        """ 
+        CREATE TABLE IF NOT EXISTS store_table( 
+        branch_code varchar, 
+        branch_name varchar
+        );
+    """
 
-cursor.execute(sql)
-connection.commit()
+    cursor.execute(sql)
+    connection.commit()
+
+def create_transaction_table():
+    sql = \
+        """
+        CREATE TABLE IF NOT EXISTS transactions_table(
+        tran_id INT PRIMARY KEY,
+        time varchar, 
+        branch_code varchar,
+        cust_id int, 
+        payment_method varchar,
+        total_price float
+        );
+    """
+
+    cursor.execute(sql)
+    connection.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#func_1()
+#func_2()
+create_store_table()
+create_transaction_table()
 connection.close()
